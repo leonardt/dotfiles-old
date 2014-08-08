f04883cf638692de3edf8d425cb7a885e57dd1f5
Leonard Truong <leonardtruong@gmail.com>
2014-08-07 22:23:57 -0700

    adding install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Link config files
+ln zsh/.zshrc ~/.zshrc
+ln .vimrc ~/.vimrc
+ln .tmux.conf ~/.tmux.conf
+
+# Install vim-plug
+mkdir -p ~/.vim/autoload
+curl -fLo ~/.vim/autoload/plug.vim \
+    https://raw.github.com/junegunn/vim-plug/master/plug.vim
