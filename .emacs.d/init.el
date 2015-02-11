06d51a472b7243565b3e70997ee46d1676f2bd30
Leonard Truong <leonardtruong@gmail.com>
2015-02-10 20:49:53 -0800

    Update emacs.d

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("b458d10c9ea0c8c465635b7b13e1bd23f04e6b696b1ca96cb2c4eca35a31641e" "013e87003e1e965d8ad78ee5b8927e743f940c7679959149bbee9a15bd286689" "9eb5269753c507a2b48d74228b32dcfbb3d1dbfd30c66c0efed8218d28b8f0dc" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
+ '(elpy-modules
+   (quote
+    (elpy-module-company elpy-module-eldoc elpy-module-flymake elpy-module-pyvenv elpy-module-yasnippet elpy-module-sane-defaults)))
+ '(elpy-test-runner (quote elpy-test-nose-runner))
+ '(magit-use-overlays nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives
@@ -11,6 +31,11 @@
     (unless (assoc package package-archive-contents)
       (package-refresh-contents))
     (package-install package)))
+
+;; tramp, for sudo access
+(require 'tramp)
+;; keep in mind known issues with zsh - see emacs wiki
+(setq tramp-default-method "ssh")
 
 (server-start)
 (setenv "EDITOR" "emacsclient")
@@ -26,6 +51,8 @@
 
 (require-package 'flx)
 (require-package 'flx-ido)
+(require-package 'ido-vertical-mode)
+(require-package 'ido-ubiquitous)
 (require 'flx-ido)
 (ido-mode 1)
 (ido-everywhere 1)
@@ -33,6 +60,19 @@
 ;; disable ido faces to see flx highlights.
 (setq ido-enable-flex-matching t)
 (setq ido-use-faces nil)
+(ido-vertical-mode 1)
+(ido-ubiquitous-mode 1)
+
+(require 'recentf)
+
+(recentf-mode t)
+
+(defun ido-recentf-open ()
+  "Use `ido-completing-read' to \\[find-file] a recent file"
+  (interactive)
+  (if (find-file (ido-completing-read "Find recent file: " recentf-list))
+      (message "Opening file...")
+    (message "Aborting")))
 
 (require-package 'smartparens)
 (require 'smartparens-config)
@@ -115,8 +155,18 @@
 (require 'smex)
 (smex-initialize)
 
+(evil-add-hjkl-bindings magit-log-mode-map 'emacs)
+(evil-add-hjkl-bindings magit-commit-mode-map 'emacs)
+(evil-add-hjkl-bindings magit-branch-manager-mode-map 'emacs
+  "K" 'magit-discard-item
+  "L" 'magit-key-mode-popup-logging)
+(evil-add-hjkl-bindings magit-status-mode-map 'emacs
+  "K" 'magit-discard-item
+  "l" 'magit-key-mode-popup-logging
+  "h" 'magit-toggle-diff-refine-hunk)
+
 (evil-leader/set-key
-  "<SPC>" 'helm-mini
+  "<SPC>" 'ido-recentf-open
   "x" 'smex
   "c" 'compile
   "r" 'recompile
@@ -127,25 +177,17 @@
   "g p s" 'magit-push
   "g p l" 'magit-pull
   "g c" 'magit-commit
-  "s p" 'projectile-switch-project)
+  "p s" 'projectile-switch-project)
 
 (define-key evil-normal-state-map (kbd "C-p") 'projectile-find-file)
 
 (require-package 'elpy)
 (elpy-enable)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(elpy-modules
-   (quote
-    (elpy-module-company elpy-module-eldoc elpy-module-flymake elpy-module-pyvenv elpy-module-yasnippet elpy-module-sane-defaults)))
- '(elpy-test-runner (quote elpy-test-nose-runner)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(require-package 'solarized-theme)
+(require-package 'flatui-theme)
+
+(load-theme 'flatui)
+
+(add-to-list 'default-frame-alist
+             '(font . "Akkurat Mono-12"))
